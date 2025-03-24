af56504d331796533f40cf68deb772aa268a04f4
EverettRichards <ehrichards9@gmail.com>
2025-03-24 10:09:33 -0700

    Added prize section code

---
+++ src/data/hack2025prizes.js
@@ -0,0 +1,8 @@
+const blank = "https://www.booksie.com/files/profiles/22/mr-anonymous_230x230.png?0000-00-00%2000:00:00"
+
+export const prizes = [
+    {
+        title: "Raspberry Pi (Mini PC)",
+        photo: "https://cdn-shop.adafruit.com/970x728/2258-01.jpg",
+    },
+]
