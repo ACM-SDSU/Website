2e0664ce7ea1ffdaf64e2b9811d6f79655905f41
EverettRichards <ehrichards9@gmail.com>
2025-05-02 17:07:39 -0700

    Fixed oopsie colors :3

--- src/components/history/Officers2024.js
+++ src/components/history/Officers2024.js
@@ -4,7 +4,7 @@ import { officers } from "../../data/officers2024";
 
 export default function Officers() {
     return (
-        <section id="officers2024" class="p-8 pb-10 bg-gray-500 text-gray-200">
+        <section id="officers2024" class="p-8 pb-10 bg-gray-900 text-gray-200">
             <h1 class="text-5xl font-bold text-center m-4 mb-8 text-acm-light-blue">2024-2025 Executive Committee</h1>
             <OfficerCards officers={officers} />
         </section>
