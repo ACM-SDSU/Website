171f6c399bac667442d967fc11f1100e7b0d9f2e
EverettRichards <ehrichards9@gmail.com>
2025-10-09 14:27:16 -0700

    Major info updates to hack website

---
+++ src/components/hackathon_nov2025/Committee.js
@@ -0,0 +1,96 @@
+import React from "react";
+
+function OfficerCards({officers}) {
+    return (
+        <div className="grid grid-cols-2 sm:grid-cols-4 lg:grid-cols-6 justify-center items-stretch gap-2 md:gap-4 m-4">
+            {officers.map((officer) => (
+                <a href={officer.url ? officer.url : `mailto:${officer.email}`} target="_blank">
+                    <div key={officer.id} className="min-h-12 min-w-16 text-center text-gray-200
+                    bg-gradient-to-r from-acm-dark-blue to-acm-dark-blue via-acm-blue
+                    p-2 md:p-4 rounded-lg flex flex-col items-center justify-center h-full"
+                    >
+                        <img src={officer.photo} class="w-24 h-24 sm:w-32 sm:h-32 md:w-40 md:h-40 xl:w-48 xl:h-48 mb-1 rounded-xl bg-gray-300"></img>
+                        <p class="text-base md:text-2xl xl:text-3xl flex items-center justify-center space-x-2 font-bold">
+                            {officer.name}
+                        </p>
+                        <p class="text-sm md:text-lg xl:text-xl">{officer.position}</p>
+                    </div>
+                </a>
+            ))}
+        </div>
+    );
+}
+
+const blank = "https://static.vecteezy.com/system/resources/previews/001/921/566/non_2x/businessman-elegant-with-infographic-presentation-free-vector.jpg"
+
+const committee = [
+    {
+        name: "Isabella King",
+        position: "Chair",
+        photo: require("../../images/officers25/bella.png"),
+    },
+    {
+        name: "Everett Richards",
+        position: "Member, Webmaster, ACM President, CTRL VP",
+        photo: require("../../images/officers25/everett.png"),
+    },
+    {
+        name: "Justin Pelak",
+        position: "Member, CTRL President, AI Club President",
+        photo: blank,
+    },
+    {
+        name: "Amelia Grevin",
+        position: "Member",
+        photo: blank,
+    },
+    {
+        name: "Enzo Weiss",
+        position: "Member",
+        photo: blank,
+    },
+    {
+        name: "Caleb Dickson",
+        position: "Member",
+        photo: blank,
+    },
+    {
+        name: "Richie Walcher",
+        position: "Member",
+        photo: blank,
+    },
+    {
+        name: "Paul Steitz",
+        position: "Member",
+        photo: blank,
+    },
+    {
+        name: "Matthew Hernandez",
+        position: "Member",
+        photo: blank,
+    },
+    {
+        name: "Patricia Alfonso",
+        position: "Member",
+        photo: blank,
+    },
+    {
+        name: "Avishka Wickramaratna",
+        position: "Member",
+        photo: blank,
+    },
+    {
+        name: "Priya Manju Muralidharan",
+        position: "Faculty Advisor",
+        photo: require("../../images/hack25/committee/manju.jpg"),
+    },
+]
+
+export default function Committee() {
+    return (
+        <section id="committee" class="pt-6 pb-0 items-center text-white">
+            <h1 class="hackheader mb-0">Planning Committee</h1>
+            <OfficerCards officers={committee}/>
+        </section>
+    );
+}
--- src/components/hackathon_nov2025/Prizes.js
+++ src/components/hackathon_nov2025/Prizes.js
@@ -1,19 +1,57 @@
 import React from "react";
-import { prizes } from "../../data/hack2025prizes";
 import {HackLogo} from "../Modules"
+
+const blank = "https://www.booksie.com/files/profiles/22/mr-anonymous_230x230.png?0000-00-00%2000:00:00"
+
+const prizes = [
+    {
+        title: "Sony WH1000-XM5 Premium Headphones",
+        photo: "https://m.media-amazon.com/images/I/51aXvjzcukL.jpg",
+        value: "$300",
+    },
+    {
+        title: "San Diego VR Experience for 4",
+        photo: "https://www.sandiegovr.com/_next/image?url=%2Flogo-light.png&w=1080&q=75",
+        value: "$250",
+    },
+    {
+        title: "Amazon Gift Card",
+        photo: "https://pisces.bbystatic.com/image2/BestBuy_US/images/products/6545/6545175_sd.jpg",
+        value: "$250",
+    },
+    // {
+    //     title: "Anker Bluetooth Speaker",
+    //     photo: "https://m.media-amazon.com/images/I/614l20nEhmL.__AC_SX300_SY300_QL70_FMwebp_.jpg",
+    // },
+    // {
+    //     title: "Gaming Mouse and Keyboard",
+    //     photo: "https://m.media-amazon.com/images/I/71AR+xtHK7L._AC_SX569_.jpg",
+    // },
+    // {
+    //     title: "STEAM Gaming Gift Card",
+    //     photo: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT-3mrll8inNYxpTqtbHlJymURw_978a_VRRQ&s",
+    // },
+]
 
 export default function Prizes({headerStyle}) {
     return (
         <section id="prizes" class="pt-6 pb-12 items-center text-white">
             <h1 class="hackheader">Prizes</h1>
-            <p class="w-4/5 lg:w-2/3 mx-auto text-center xl:text-2xl">Participants who develop creative, high-impact projects, are eligible to receive the following prizes.</p>
-            <div className="grid grid-cols-2 lg:grid-cols-3  md:w-[90%] lg:w-2/3 mx-auto justify-center gap-4 m-4">
+            <p class="w-4/5 lg:w-2/3 mx-auto text-center md:text-xl xl:text-2xl">
+            Participants who develop creative, high-impact projects, are eligible to receive the following prizes.
+            </p>
+            <div className="grid grid-cols-2 lg:grid-cols-3  md:w-[70%] lg:w-1/2 mx-auto justify-center gap-4 m-4">
                 {prizes.map((prize) => (
-                    <div key={prize.id} className="min-h-32 min-w-16 text-center text-gray-200 bg-gradient-to-r from-acm-orange to-acm-orange via-acm-yellow p-2 rounded-2xl flex flex-col items-center justify-center">
-                        <img src={prize.photo} class="h-28 xl:h-36 w-auto mx-auto mb-1 rounded-xl"></img>
-                        <p class="text-xl flex items-center justify-center space-x-2 font-bold drop-shadow-[1px_1px_2px_black]">
+                    <div key={prize.id} className="min-h-32 min-w-16 text-center text-gray-200 p-2 bg-black/50 rounded-2xl border-2 border-acm-orange flex flex-col items-center justify-center">
+                        <img src={prize.photo} class="h-32 xl:h-40 w-auto mx-auto mb-1 rounded-xl bg-none"></img>
+                        <p class="text-xl flex items-center justify-center space-x-2 font-bold mt-2">
                             {prize.title}
                         </p>
+                        {prize.value && (
+                            <p class="text-lg flex items-center justify-center space-x-2">
+                                ({prize.value} value)
+                            </p>
+                        )}
                     </div>
                 ))}
             </div>
